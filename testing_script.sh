7f771fea6bb929ebd4565c5298c7bca8061672b6
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2022-09-22 21:12:48 -0400

    Fixed testing_script -> still working on it

--- testing_script.sh
+++ testing_script.sh
@@ -1,4 +1,9 @@
 #! bin.bash
+if [ $# -eq 0 ]
+  then
+    echo "./testing_script.sh <seed> <skill>"
+  exit
+fi
 # Get all maps with json extension from maps directory and save them to txt file
 find ./maps -type f -name "*.json" > output.txt
 
@@ -6,6 +11,6 @@ find ./maps -type f -name "*.json" > output.txt
 filename='output.txt'
 n=1
 while read line; do
-python main.py --players 7 --seed 1234 --skill 100 --map $line
+python main.py --players 7 --seed $1 --skill $2 --map $line
 n=$((n+1))
 done < $filename
