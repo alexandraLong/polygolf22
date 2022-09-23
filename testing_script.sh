c0b6f316db2a61a772b085ddaa664907d625be00
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2022-09-22 21:05:04 -0400

    First prototype of running each map in maps directory with our code

--- testing_script.sh
+++ testing_script.sh
@@ -1,10 +1,11 @@
 #! bin.bash
+# Get all maps with json extension from maps directory and save them to txt file
 find ./maps -type f -name "*.json" > output.txt
 
+# Iterate through each map and run tha game with our code
 filename='output.txt'
 n=1
 while read line; do
-# reading each line
-echo "Line No. $n : $line"
+python main.py --players 7 --seed 1234 --skill 100 --map $line
 n=$((n+1))
 done < $filename
